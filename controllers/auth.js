ab92aea759416efd1ba5e28b5b20d7ee26874468
Aditya Yadav <adityadav023>
2022-02-13 18:29:30 +0530

    Generated jwt token in register controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,20 @@
 const bcrypt = require('bcryptjs')
 const {StatusCodes} = require('http-status-codes')
 const User = require('../models/User')
+const jwt = require('jsonwebtoken')
 
 const register = async (req,res)=>{
 
     // upload to db
     const user = await User.create({...req.body})
+    //generating token
+    const token = jwt.sign({userId: user._id,
+                     name: user.name},'jwtSecret',
+                     {
+                         expiresIn:'30d'
+                     })
 
-    res.status(StatusCodes.CREATED).json({user})
+    res.status(StatusCodes.CREATED).json({user:{name:user.name}, token})
 
 
 }
