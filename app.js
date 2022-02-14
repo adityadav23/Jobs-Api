a09a84dc7d67c166f2045a27ec7bf21645672741
Aditya Yadav <adityadav023>
2022-02-15 00:53:19 +0530

    Added security libraries

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 require('dotenv').config();
 require('express-async-errors');
+
+//extra security packages
+const helmet = require('helmet')
+const  cors = require('cors')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
+
 const express = require('express');
 const app = express();
 const connectDB = require('./db/connect')
@@ -8,11 +15,22 @@ const authenticateUser = require('./middleware/authentication')
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+app.set('trust proxy', 1)
+app.use(
+  rateLimiter({
+    windowMs: 15*60*1000,    //15 minutes
+    max: 100 
+  })
+)
+app.use(express.json())
+app.use(helmet())
+app.use(cors())
+app.use(xss())
+
 //routes 
 const authRouter = require('./routes/auth')
 const jobsRouter = require('./routes/jobs')
 
-app.use(express.json())
 
 // routes
 app.use('/api/v1/auth', authRouter)
