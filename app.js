ea3dd27c115e809470725e4d7cd9f09ab880612c
adityadav23 <72222769+adityadav23@users.noreply.github.com>
2022-02-15 23:01:28 +0530

    swagger docs added

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ const  cors = require('cors')
 const xss = require('xss-clean')
 const rateLimiter = require('express-rate-limit')
 
+
+// Swagger
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
 const express = require('express');
 const app = express();
 const connectDB = require('./db/connect')
@@ -30,6 +36,13 @@ app.use(xss())
 //routes 
 const authRouter = require('./routes/auth')
 const jobsRouter = require('./routes/jobs')
+
+
+
+app.get('/', (req, res) => {
+  res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
+});
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 
 // routes
